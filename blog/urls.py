6541a58374bea589bb99dae40636c43c6d8dc05e
fatima <fatimagulomova@gmail.com>
2020-02-12 11:35:58 +0200

    add few html elements, add nginx and finished lesons by DjangaTutorial

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.shortcuts import render
+from .models import Post
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
 ]
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
